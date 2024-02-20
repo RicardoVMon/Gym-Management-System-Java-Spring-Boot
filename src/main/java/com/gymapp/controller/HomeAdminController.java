012a88b7fe5caa54a310588ba2d2d5481c03d937
Daniel Vega Marvez <87838943+Chuz047@users.noreply.github.com>
2024-02-20 17:21:19 -0600

    Navbar y Sidebars para ambos niveles de acceso (User/Admin)

---
+++ src/main/java/com/gymapp/controller/HomeAdminController.java
@@ -0,0 +1,22 @@
+package com.gymapp.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/*
+Este controlador y "HomeUserController" se convertirán en un solo "HomeController" cuando
+aprendamos a manejar diferentes usuarios con condicionales de ThymeLeaf.
+Más especificaciones en "homeUser.html" y "homeAdmin.html".
+*/
+
+@Controller
+public class HomeAdminController {
+
+    @RequestMapping("/homeAdmin.html")
+    public String page(Model model) {
+        model.addAttribute("attribute", "value");
+        return "homeAdmin";
+    }
+
+}
