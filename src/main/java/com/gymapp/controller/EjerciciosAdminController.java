3d909b29753878d53252b9415fe00472fc43200f
Ricardo Vargas <rvargas20226@ufide.ac.cr>
2024-03-02 17:48:58 -0600

    Se agrega sección de usuariosAdmin y se mejora organización de código

--- src/main/java/com/gymapp/controller/EjerciciosAdminController.java
+++ src/main/java/com/gymapp/controller/EjerciciosAdminController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class EjerciciosAdminController {
     
-    @RequestMapping("/ejercicios_admin")
+    @RequestMapping("/ejerciciosAdmin")
     public String page(Model model) {
         model.addAttribute("attribute", "value");
         return "ejercicios_admin";
