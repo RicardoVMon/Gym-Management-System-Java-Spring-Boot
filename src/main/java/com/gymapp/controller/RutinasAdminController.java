af4d8204184ec0d492060dd77673d48be62897d3
Ricardo Vargas <rvargas20226@ufide.ac.cr>
2024-03-04 17:43:43 -0600

    Se agrega sección de Rutinas para ambos roles corregidas

--- src/main/java/com/gymapp/controller/RutinasAdminController.java
+++ src/main/java/com/gymapp/controller/RutinasAdminController.java
@@ -2,15 +2,30 @@ package com.gymapp.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class RutinasAdminController {
 
-    @RequestMapping("/rutinas-admin")
+    @GetMapping("/rutinasAdmin")
     public String rutinasAdmin(Model model) {
+        return "/rutinas-admin/rutinas-1";
+    }
+    
+    @GetMapping("/rutinasAdmin/usuario")
+    public String rutinasAdminUsuario(Model model) {
+        return "/rutinas-admin/rutinas-2";
+    }
+    
+    @GetMapping("/rutinasAdmin/usuario/rutina")
+    public String rutinasAdminRutina(Model model) {
+        return "/rutinas-admin/rutinas-3";
+    }
+    
+    @GetMapping("/rutinasAdmin/usuario/rutina/agregarEjercicio")
+    public String rutinasAdminEjercicio(Model model) {
         model.addAttribute("attribute", "value");
-        return "rutinas-admin";
+        return "/rutinas-admin/rutinas-4";
     }
 
 }
