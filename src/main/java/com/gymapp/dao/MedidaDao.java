4a6fce2677610d15ff134051305df4f0b5c9c2f4
StephanieFallas <sfallas20409@ufide.ac.cr>
2024-03-24 22:18:33 -0600

    Conectar edición de medidas con la BD para leer, guardar y borrar

--- src/main/java/com/gymapp/dao/MedidaDao.java
+++ src/main/java/com/gymapp/dao/MedidaDao.java
@@ -2,11 +2,15 @@ package com.gymapp.dao;
 
 import com.gymapp.domain.Medida;
 import com.gymapp.domain.Usuario;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.jpa.repository.support.JpaRepositoryImplementation;
 
 /*Funciones requeridas por DAO de medidas: CRUD*/
 public interface MedidaDao extends JpaRepositoryImplementation<Medida, Long> {
 
     Medida findByUsuario(Usuario usuario);
-
+    
+    @Query("SELECT m FROM Medida m WHERE m.usuario.idUsuario = ?1")
+    Medida findByIdUsuario(Long idUsuario);
+    
 }
